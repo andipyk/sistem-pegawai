4bdfc2c74d8ea2e7104ebae55e0336a53a3e109f
Andi Syafrianda <andisyafrianda@gmail.com>
2023-05-02 14:11:06 +0700

    controller

---
+++ app/Http/Controllers/JabatanPegawaiController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JabatanPegawai;
+use Illuminate\Http\Request;
+
+class JabatanPegawaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JabatanPegawai $jabatanPegawai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JabatanPegawai $jabatanPegawai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, JabatanPegawai $jabatanPegawai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JabatanPegawai $jabatanPegawai)
+    {
+        //
+    }
+}
